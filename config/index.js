1d960406b19a85d0c67f0590f87b88da614c55c7
silianpan <2480621579@qq.com>
2017-05-14 14:03:48 +0800

    What: static resource
    Why：
    
    *
    
    How:
    
    *

--- config/index.js
+++ config/index.js
@@ -7,7 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './', //生产环境assetsPublicPath: '/'
+    staticPath:'./', //生产环境 staticPath:''
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -27,6 +28,7 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    staticPath:'/static/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
